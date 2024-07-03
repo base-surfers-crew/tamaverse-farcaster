538586fd2f12cedc5e3f8133d79f553b9b914716
Daniil <boiko.81@list.ru>
2024-07-03 16:04:26 +0300

    rollback

--- frontend/app/api/get-tx-data/route.ts
+++ frontend/app/api/get-tx-data/route.ts
@@ -22,48 +22,37 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     })
 
     const mintResponseData = await mintResponse.json();
-    // console.log( new NextResponse(JSON.stringify(mintResponseData), { status: 200 }))
-    const test = {
-      chainId: mintResponseData.chainId,
-      method: mintResponseData.method,
+    const txPayload = {
+      chainId: "eip155:84532",
+      method: "eth_sendTransaction",
       params: {
-        abi: mintResponseData.params.abi,
-        to: mintResponseData.params.to,
-        data: mintResponseData.params.data,
-        value: mintResponseData.params.value,
-      }
-    }
-    // const txPayload = {
-    //   chainId: "eip155:84532",
-    //   method: "eth_sendTransaction",
-    //   params: {
-    //     abi: [
-    //       {
-    //           "constant": false,
-    //           "inputs": [
-    //             {
-    //               "name": "_to",
-    //               "type": "address"
-    //             },
-    //             {
-    //               "name": "_value",
-    //               "type": "uint256"
-    //             }
-    //           ],
-    //           "name": "mint",
-    //           "outputs": [],
-    //           "payable": false,
-    //           "stateMutability": "nonpayable",
-    //           "type": "function"
-    //         }
-    //     ],
-    //     to: "0x00000000fcCe7f938e7aE6D3c335bD6a1a7c593D",
-    //     data: "0x783a112b0000000000000000000000000000000000000000000000000000000000000e250000000000000000000000000000000000000000000000000000000000000001",
-    //     value: "984316556204476",
-    //   },
-    // };
+        abi: [
+          {
+              "constant": false,
+              "inputs": [
+                {
+                  "name": "_to",
+                  "type": "address"
+                },
+                {
+                  "name": "_value",
+                  "type": "uint256"
+                }
+              ],
+              "name": "mint",
+              "outputs": [],
+              "payable": false,
+              "stateMutability": "nonpayable",
+              "type": "function"
+            }
+        ],
+        to: "0x00000000fcCe7f938e7aE6D3c335bD6a1a7c593D",
+        data: "0x783a112b0000000000000000000000000000000000000000000000000000000000000e250000000000000000000000000000000000000000000000000000000000000001",
+        value: "984316556204476",
+      },
+    };
     return new NextResponse(
-      JSON.stringify(test), 
+      JSON.stringify(txPayload), 
       { status: 200, headers: { 'Content-Type': 'application/json' }  });
   } catch {
     const errorMeta = await errorHandler('Error on mint NFT')
